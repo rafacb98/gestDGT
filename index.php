8a283a0066bb2af3832c6e4d740e3df8ba38d05a
Rafa <rafaelcarrillobonilla@gmail.com>
2020-05-05 14:02:42 +0200

    Crea la pagina de inicio de sesión con sus estilos

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	  <meta name="author" content="Rafael Carrillo Bonilla">
+	  <meta name="description" content="GestDGT+">
+	  <meta name="lang" content="es-ES" />
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <link rel='stylesheet' type="text/css" href='css/estilosiniciosesion.css'>
+    <link rel="icon" type="image/png" href="img/logotipo3png.png">
+    <link href="https://fonts.googleapis.com/css?family=Pathway+Gothic+One&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/4a9d5317b6.js" crossorigin="anonymous"></script>
+    <script src='js/jquery-3.1.1.js'></script>
+    <script src='js/script1.js'></script>
+    <title>GestDGT+</title>
+</head>
+
+<body class="centro">
+  <main class="login centro">
+    <p>
+      <img class="logo" src='img/logotipo3png.png'/>
+    </p>
+ 
+    <form class="login-form centro" method='post' action='prueba.php'>
+      <p id="test" class="usuario">
+        <label for="usuario" class="labelusuclave centro">
+          <i class="fas fa-user"></i>
+        </label>
+        <input id="usuario" placeholder="Usuario"  type="text">
+      </p>
+
+      <p class="clave">
+        <label for="clave" class="labelusuclave centro">
+          <i class="fas fa-lock"></i>
+        </label>
+        <input id="clave" placeholder="Clave" type="password">
+      </p>
+      <button type='submit' id="btnentrar" class="centro">
+        Entrar
+      </button>
+    </form>
+
+    
+</main> 
+
+</body> 
+</html>
