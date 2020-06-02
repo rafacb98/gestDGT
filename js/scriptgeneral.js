0fb421f96450b214de624931dadc94b29cf5a8ab
Rafa <rafaelcarrillobonilla@gmail.com>
2020-06-02 12:14:01 +0200

    Soluciona fallo de la ventana modal, ahora el fondo de debajo ocupa el 100% y cambia el color

--- js/scriptgeneral.js
+++ js/scriptgeneral.js
@@ -119,10 +119,14 @@ $(document).ready(function () {
 
        document.getElementById("verpuntos").addEventListener("click",function(){
         document.getElementsByClassName("fondo_transparente")[0].style.display="block" 
+        
+       
+       
         return false
      })
      document.getElementsByClassName("modal_cerrar")[0].addEventListener("click",function(){
          document.getElementsByClassName("fondo_transparente")[0].style.display="none" 
+      
      })
 
 
